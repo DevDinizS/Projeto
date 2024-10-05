e37e4a11deaa0601de1af8740c7498d7edc742ff
Filipe Diniz <127323099+DevDinizS@users.noreply.github.com>
2024-10-04 22:05:33 -0300

    Modo Dark/Light

--- menu.js
+++ menu.js
@@ -3,7 +3,8 @@ let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
 let botao = document.getElementById('botao')
 let body = document.querySelector('body')
-
+var imgDark = "imagens/Filipe-wb-150-t.png";
+var imgLight = "imagens/Filipe-150bw.png";
 
 btnMenu.addEventListener('click', () => {
     menu.classList.add('menu-abre')
@@ -20,4 +21,14 @@ overlay.addEventListener('click', () => {
 botao.addEventListener('click', () => {
     botao.classList.toggle('light')
     body.classList.toggle('light')
+    footer.classList.toggle('light')
 })
+
+function trocar() {
+    document.getElementById('figura').src = imgDark;
+    document.getElementById('figura1').src = imgDark;
+    
+    let aux = imgDark;
+    imgDark = imgLight;
+    imgLight = aux;
+}
